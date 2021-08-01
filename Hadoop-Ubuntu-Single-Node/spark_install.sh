eb133a12f42e8183d20e1818825afee6c4cbb1a3
Suraj N <surajn@mactores.com>
2021-08-02 00:06:01 +0530

    Typo

--- Hadoop-Ubuntu-Single-Node/spark_install.sh
+++ Hadoop-Ubuntu-Single-Node/spark_install.sh
@@ -15,6 +15,9 @@ mkdir -p $HOME/spark-3.1.2-bin-hadoop2.7
 
 
 install_spark(){
+sudo apt update -y
+sudo apt install scala -y
+
 cd $HOME
 wget $SPARK_URL
 tar -xvzf spark-3.1.2-bin-hadoop2.7.tgz
@@ -26,7 +29,7 @@ configure_env_vars(){
 
 echo "export PATH=\$PATH:$HOME/scripts" >> $SHELL_PROFILE
 echo "export SPARK_HOME=$HOME/spark-3.1.2-bin-hadoop2.7" >> $HOME/.bashrc
-echo "export PATH=PATH:$HOME/spark-3.1.2-bin-hadoop2.7/bin:$HOME/spark-3.1.2-bin-hadoop2.7/sbin" >> ~$HOME/.bashrc
+echo "export PATH=PATH:$HOME/spark-3.1.2-bin-hadoop2.7/bin:$HOME/spark-3.1.2-bin-hadoop2.7/sbin" >> $HOME/.bashrc
 echo "export PYSPARK_PYTHON=/usr/bin/python3" >> $HOME/.bashrc
 echo "export PYSPARK_DRIVER_PYTHON=python3" >> $HOME/.bashrc
 echo "export PYSPARK_DRIVER_PYTHON_OPTS=\"notebook\"" >> $HOME/.bashrc
@@ -45,7 +48,7 @@ start_spark()
 sudo -u hduser $SPARK_HOME/sbin/start-master.sh
 sudo -u hduser $SPARK_HOME/sbin/start-workers.sh spark://localhost:7077
 
-sudo -u hduser $SPARK_HOME/bin/spark-submit --master yarn --executor-memory 1G --driver-memory 1G --conf spark.sql.warehouse.dir=\"file:///tmp/spark-warehouse\" --packages com.databricks:spark-csv_2.11:1.5.0 --packages com.amazonaws:aws-java-sdk-pom:1.10.34 --packages org.apache.hadoop:hadoop-aws:2.7.3 /vagrant/test-spark.sh
+sudo -u hduser $SPARK_HOME/bin/spark-submit --master yarn --executor-memory 1G --driver-memory 1G --conf spark.sql.warehouse.dir=\"file:///tmp/spark-warehouse\" --packages com.databricks:spark-csv_2.11:1.5.0 --packages com.amazonaws:aws-java-sdk-pom:1.10.34 --packages org.apache.hadoop:hadoop-aws:2.7.3 /vagrant/test_spark.sh
 }
 
 setup_variables
