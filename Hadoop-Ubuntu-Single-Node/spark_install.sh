94a13a62b5a3b1e3031c24834037fc1c85f2c2e9
Suraj N <surajn@mactores.com>
2021-08-06 04:35:14 +0530

    Added Kafka box

--- Hadoop-Ubuntu-Single-Node/spark_install.sh
+++ Hadoop-Ubuntu-Single-Node/spark_install.sh
@@ -48,7 +48,7 @@ start_spark()
 sudo -u hduser $SPARK_HOME/sbin/start-master.sh
 sudo -u hduser $SPARK_HOME/sbin/start-workers.sh spark://localhost:7077
 
-sudo -u hduser $SPARK_HOME/bin/spark-submit --master yarn --executor-memory 1G --driver-memory 1G --conf spark.sql.warehouse.dir=\"file:///tmp/spark-warehouse\" --packages com.databricks:spark-csv_2.11:1.5.0 --packages com.amazonaws:aws-java-sdk-pom:1.10.34 --packages org.apache.hadoop:hadoop-aws:2.7.3 /vagrant/test_spark.sh
+sudo -u hduser $SPARK_HOME/bin/spark-submit --master yarn --executor-memory 1G --driver-memory 1G --conf spark.sql.warehouse.dir=\"file:///tmp/spark-warehouse\" --packages com.databricks:spark-csv_2.11:1.5.0 --packages com.amazonaws:aws-java-sdk-pom:1.10.34 --packages org.apache.hadoop:hadoop-aws:2.7.3 /vagrant/test_spark.py
 }
 
 setup_variables
