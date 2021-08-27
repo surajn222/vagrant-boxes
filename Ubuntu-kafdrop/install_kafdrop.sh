da60f647e06a24e190254ec2fe5526292cace1d8
snathani <snathani@infoblox.com>
2021-08-27 11:24:51 +0530

    updated kafka and kafdrop

---
+++ Ubuntu-kafdrop/install_kafdrop.sh
@@ -0,0 +1,16 @@
+#Login to the machine and run this script with root account
+apt-get remove openjdk* -y
+apt-get install openjdk-11-jdk -y
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/" >> /root/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/" >> ~/.bashrc
+cd /opt
+git clone https://github.com/obsidiandynamics/kafdrop.git
+ls
+cd kafdrop/
+ls
+pwd
+mvn clean -Dskiptests package install
+apt-get install maven -y
+mvn clean -Dskiptests package install
+java --add-opens=java.base/sun.nio.ch=ALL-UNNAMED -jar target/kafdrop-3.28.0-SNAPSHOT.jar --kafka.brokerConnect=10.0.2.2:9093 &> kafdrop.log &
+
