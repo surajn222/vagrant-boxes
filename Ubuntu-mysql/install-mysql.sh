a3af509c53811a458fe2573fae673fbcbdaeb101
Suraj N <surajn@mactores.com>
2022-02-22 18:46:17 +0530

    Updated Ubuntu mysql

--- Ubuntu-mysql/install-mysql.sh
+++ Ubuntu-mysql/install-mysql.sh
@@ -3,13 +3,11 @@ apt-get install mysql-server -y
 sudo service mysql restart
 
 #Make mysql listen on 0.0.0.0
-#vim /etc/mysql/mysql.conf.d/mysqld.cnf
+#vim /etc/mysql/mysql.conf.d/mysqld.cnfi
 #Change bind-address to 0.0.0.0
 #sudo service mysql restart
 #netstat -peanut | grep 3306
 
 #Create new user
 #mysql -e "CREATE USER 'root'@'%' IDENTIFIED BY 'toor';"
-#mysql -e "GRANT ALL ON *.* TO 'root'@'%'"
-#mysql -e "flush privileges;"
-#sudo service mysql restart
+
